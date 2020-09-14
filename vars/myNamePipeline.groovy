deabc46d1c8773e97031cb3d16865a1b719e8cb7
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-14 12:07:45 -0500

    adding new shared pipeline

---
+++ vars/myNamePipeline.groovy
@@ -0,0 +1,33 @@
+def call (body) {
+  // Estas lineas siempre deben estar en el pipeline, me pasan las variables que tenga definicas a pipelineParams
+  def pipelineParams = [:]
+  body.resolverStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
+  pipelineParams.each { println(it) }
+
+  // Definicion de variables por defecto, si no estan en el Jenkinsfile
+  pipelineParams.pipelineTemplateName = This.getClass().toString.split(' ')[1]
+
+  /*
+  Pipeline structure
+  echo Jenkins variables
+  echo Default variables
+  */
+
+  pipeline {
+    agent any
+    stages {
+      stage ('echo Jenkins variables') {
+        steps {
+          sh ("echo my name is ${pipelineParams.name} and im ${pipelineParams.age} years old")
+        }
+      }
+      stage ('echo Default variables') {
+        steps {
+          sh ("echo name of the template is ${pipelineParams.pipelineTemplateName}")
+        }
+      }
+    }
+  }
+}
