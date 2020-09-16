e4bcaa4d06ed7f1e971b0d6c8fad44af2dd02321
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-16 09:22:29 -0500

    fixing summary

--- vars/slack_notification_pipeline.groovy
+++ vars/slack_notification_pipeline.groovy
@@ -1,4 +1,4 @@
 def call(String buildStatus = 'FAILURE', String customMessage = null){
-  summary = "${Message from Jenkins Pipeline} (<${env.BUILD_URL}|Open>)"
+  summary = "Message from Jenkins Pipeline (<${env.BUILD_URL}|Open>)"
   slackSend color: "good", message: summary
 }
